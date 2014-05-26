0877aacc89fb8be1ad46f6f171f000a70c40b71d
Jacinda Zhong <jacindaz@gmail.com>
2014-05-26 09:29:03 -0400

    Creates print_movie method to print information for an individual movie
     - takes in the hash for a particular movie as a parameter
     - pulls out the appropriate information into a variable
     - calculates the critic/audience average score
     - uses the variables to print the information

--- in_theaters.rb
+++ in_theaters.rb
@@ -1,3 +1,34 @@
 require 'json'
 
+
 movie_data = JSON.parse(File.read('in_theaters.json'))
+
+
+def movie_info(movie_data)
+  puts "In Theaters Now:"
+  puts nil
+
+  movies = movie_data["movies"]
+  links = movie_data["links"]
+  link_template = movie_data["link_template"]
+
+
+
+
+end
+movie_info(movie_data)
+
+
+
+def print_movie(movie_hash)
+  title = movie_hash["title"]
+  mpaa_rating = movie_hash["mpaa_rating"]
+
+  critic_score = movie_hash["ratings"]["critics_score"]
+  audience_score = movie_hash["ratings"]["audience_score"]
+  average_scores = (critic_score + audience_score) / 2
+
+  puts "#{average_scores} - #{title} (#{mpaa_rating})"
+
+end
+#print_movie(movie_data["movies"][0])
